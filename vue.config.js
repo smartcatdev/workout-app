127e9f772ce0d8bfb172f8be91709a23f5b80668
smartcatdev <bilal@smartcat.ca>
2020-11-13 07:52:53 -0500

    removed pwa display prop from webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 // vue.config.js
 module.exports = {
-    pwa: {
-        manifestOptions: {
-            display: 'fullscreen'
-        }
-    },
     chainWebpack: (config) => {
         config.plugins.delete('prefetch')
     },
