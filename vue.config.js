5cb296af5e2a4ae8ce07f6cac385cd68fbc744cb
smartcatdev <bilal@smartcat.ca>
2020-11-13 19:48:49 -0500

    re-added fullscreen display property

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 // vue.config.js
 module.exports = {
+    pwa: {
+        manifestOptions: {
+            display: 'fullscreen'
+        }
+    },
     chainWebpack: (config) => {
         config.plugins.delete('prefetch')
     },
